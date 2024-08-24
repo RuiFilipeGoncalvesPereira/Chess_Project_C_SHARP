e21044bcbf1eea3f9720a6d80983f01f610a3251
RuiFilipeGoncalvesPereira <rui6.319@hotmail.com>
2024-08-24 16:23:01 +0100

    Project Created

---
+++ Xadrez_Console/Program.cs
@@ -0,0 +1,48 @@
+﻿using Create_Personalizated_Exceptions.Entities;
+using System;
+using Create_Personalizated_Exceptions.Entities.Exceptions;
+
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*try
+            {
+                Console.Write("Room number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/YYYY): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservations reservation = new Reservations(number, checkIn, checkOut);
+                Console.WriteLine(reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation");
+                Console.Write("Check-in date (dd/MM/YYYY): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in reservation" + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }*/
+        }
+    }
+}
