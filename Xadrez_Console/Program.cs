3144fa6631c047a4ca3761532182aa78ff798356
RuiFilipeGoncalvesPereira <rui6.319@hotmail.com>
2024-08-27 12:17:15 +0100

    Start to Implement Board

--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -3,56 +3,23 @@ using System;
 
 
 
-namespace Course
+namespace Xadrez_Console
 {
     class Program
     {
         static void Main(string[] args)
         {
 
-            Posicao P;
+            /* Posicao P;
+             P = new Posicao(3, 4);
+             Console.WriteLine("Posição: " + P);
+             Console.ReadLine();*/
 
-            P = new Posicao(3, 4);
-
-            Console.WriteLine("Posição: " + P);
+            tabuleiro tab = new tabuleiro(8, 8);
 
             Console.ReadLine();
             
-            
-            /*try
-            {
-                Console.Write("Room number: ");
-                int number = int.Parse(Console.ReadLine());
-                Console.Write("Check-in date (dd/MM/YYYY): ");
-                DateTime checkIn = DateTime.Parse(Console.ReadLine());
-                Console.Write("Check-out date (dd/MM/yyyy): ");
-                DateTime checkOut = DateTime.Parse(Console.ReadLine());
 
-                Reservations reservation = new Reservations(number, checkIn, checkOut);
-                Console.WriteLine(reservation);
-
-                Console.WriteLine();
-                Console.WriteLine("Enter data to update the reservation");
-                Console.Write("Check-in date (dd/MM/YYYY): ");
-                checkIn = DateTime.Parse(Console.ReadLine());
-                Console.Write("Check-out date (dd/MM/yyyy): ");
-                checkOut = DateTime.Parse(Console.ReadLine());
-
-                reservation.UpdateDates(checkIn, checkOut);
-                Console.WriteLine("Reservation: " + reservation);
-            }
-            catch (DomainException e)
-            {
-                Console.WriteLine("Error in reservation" + e.Message);
-            }
-            catch (FormatException e)
-            {
-                Console.WriteLine("Format error: " + e.Message);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Unexpected error: " + e.Message);
-            }*/
         }
     }
 }
